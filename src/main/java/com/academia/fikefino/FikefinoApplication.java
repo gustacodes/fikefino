eab31982b2710f16cd8553309f706e33f2f714e8
Gustavo Alencar <gustavorique@live.com>
2023-05-26 02:33:53 -0300

    Commit initial

---
+++ src/main/java/com/academia/fikefino/FikefinoApplication.java
@@ -0,0 +1,13 @@
+package com.academia.fikefino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FikefinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FikefinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/academia/fikefino/FikefinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academia.fikefino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FikefinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
